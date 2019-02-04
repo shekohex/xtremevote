3988ea08d36e82d482fa9b44239c9f5923fada7b
Shady Khalifa <shekohex@gmail.com>
2019-02-04 19:20:24 +0200

    support old mysql servers

--- src/migrations.rs
+++ src/migrations.rs
@@ -4,8 +4,8 @@ use diesel_migrations::{
 use rocket_contrib::databases::diesel::connection::SimpleConnection;
 
 const ALL_MIGRATIONS: &[&Migration] = &[&EmbeddedMigration {
-  version: "20190203204345",
-  up_sql: include_str!("../migrations//20190203204345_create_votes/up.sql"),
+  version: "20190203204346",
+  up_sql: include_str!("../migrations//20190203204346_create_votes/up.sql"),
 }];
 
 struct EmbeddedMigration {
